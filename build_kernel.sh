ef0fcb7e10399c79d4c0f169a546260708e27078
phuongminh.dang@gmail.com <phuongminh.dang@gmail.com>
2016-11-04 16:50:17 +0700

    Added travis-ci project

---
+++ build_kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://github.com/pdtechvn/toolschain/raw/master/arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz
+tar -xvf arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz
+
+echo [Build kernel...]
+ARCH_TYPE=arm
+CROSS_COMPILE_TYPE=`pwd`/4.9.3/bin/arm-none-linux-gnueabihf-
+make ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE jf4418_linux_defconfig
+make ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE uImage
+
