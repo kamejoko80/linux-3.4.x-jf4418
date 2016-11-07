1427b39fc09b5849e256c86f9c14fabc835ecd5e
phuongminh.dang@gmail.com <phuongminh.dang@gmail.com>
2016-11-07 10:45:23 +0700

    updated travis-ci

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,6 +6,6 @@ tar -xvf arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz
 echo [Build kernel...]
 ARCH_TYPE=arm
 CROSS_COMPILE_TYPE=`pwd`/4.9.3/bin/arm-none-linux-gnueabihf-
-make ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE jf4418_linux_defconfig
-make ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE uImage
+make -j8 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE jf4418_linux_defconfig
+make -j8 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE uImage
 
