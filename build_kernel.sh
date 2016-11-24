69b275c3ecaa21c281f33d0002e077e44756f012
kamejoko80 <phuongminh.dang@gmail.com>
2016-11-24 10:05:45 +0700

    update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+if [ -f "uboot-mkimage" ]; then
+echo "uboot-mkimage folder exist"
+else
 git clone https://github.com/pdtechvn/uboot-mkimage.git
+fi
+
 cd uboot-mkimage
 make
 export PATH=$PATH:$PWD
 cd ../
 
+if [ -f "arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz" ]; then
+echo "toolchains exist"
+else
 wget https://github.com/pdtechvn/toolschain/raw/master/arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz
 tar -xvf arm-cortexa9-linux-gnueabihf-4.9.3.tar.xz
+fi
 
 echo [Build kernel...]
 ARCH_TYPE=arm
 CROSS_COMPILE_TYPE=`pwd`/4.9.3/bin/arm-none-linux-gnueabihf-
-make -j8 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE jf4418_linux_defconfig
-make -j8 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE uImage
-make -j8 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE modules
-
+make -j2 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE jf4418_linux_defconfig
+make -j2 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE uImage
+make -j2 ARCH=$ARCH_TYPE CROSS_COMPILE=$CROSS_COMPILE_TYPE modules
